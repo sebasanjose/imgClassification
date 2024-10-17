d03adef9a582ba8795cfcdc368b0544b634725a4
sebasanjose <sebaenusa@gmail.com>
2024-10-17 08:58:07 -0700

    initial commit

---
+++ imgClassif.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam
+# Visualization tools
+import torchvision
+import torchvision.transforms.v2 as transforms
+import torchvision.transforms.functional as F
+import matplotlib.pyplot as plt
+
